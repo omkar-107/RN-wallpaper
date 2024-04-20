962f724f60ad411448c24ea4bdf4f867352a4acf
omkar-107 <salunkheomkar107@gmail.com>
2024-04-20 19:11:56 +0530

    Onboarding Screen done

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
+import { View, Text, ScrollView,Image } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import  CustomButton from "../components/CustomButton";
+import { Redirect, router } from "expo-router";
 
-export default function index() {
+export default function home() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-    <Text>Open up App.js to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
-  )
+    <SafeAreaView className="bg-slate-800 h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center min-h-[95vh] px-4">
+        <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+            <Text className="text-white text-4xl font-bold  my-4 ">
+                Welcome to <Text className="text-orange-500">Picks</Text>
+            </Text>
+            <Text className="text-white text-lg  ">
+                A wallpaper app for your phone
+            </Text>
+           <View className ="w-full my-8">
+           <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full my-4"
+          />
+           </View>
+           <StatusBar backgroundColor="#161622" style="light" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
