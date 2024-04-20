8b5abbf5a4afa65c5039fa179156e01711e11c9f
omkar-107 <salunkheomkar107@gmail.com>
2024-04-20 18:10:28 +0530

    Template for react-native app

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+// import { StatusBar   } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+import {Slot} from 'expo-router' 
+
+export default function App() {
+  return (
+   <Slot></Slot>
+  );
+}
+
+
---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+  )
+}
